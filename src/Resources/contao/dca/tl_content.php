360726dc824775f7d346837eb665fa05b9d7b84d
Dennis Erdmann <dennis@erdmann-freunde.de>
2018-08-09 15:33:44 +0200

    add link fields for primary and secondary buttons

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -20,7 +20,124 @@
 $GLOBALS['TL_DCA']['tl_content']['palettes']['hero'] = '{type_legend},type,headline;'
                                                        . '{text_legend},text;'
                                                        . '{image_legend},addImage;'
+                                                       . '{link_primary_legend},urlPrimary,targetPrimary,linkTitlePrimary,titleTextPrimary,relPrimary,linkClassPrimary;'
+                                                       . '{link_secondary_legend},urlSecondary,targetSecondary,linkTitleSecondary,titleTextSecondary,relSecondary,linkClassSecondary;'
                                                        . '{template_legend:hide},customTpl;'
                                                        . '{protected_legend:hide},protected;'
                                                        . '{expert_legend:hide},guests,cssID,space;'
                                                        . '{invisible_legend:hide},invisible,start,stop';
+                                                       
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['urlPrimary'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['MSC']['url'],
+	'exclude'                 => true,
+	'search'                  => true,
+	'inputType'               => 'text',
+	'eval'                    => array('rgxp'=>'url', 'decodeEntities'=>true, 'maxlength'=>255, 'dcaPicker'=>true, 'tl_class'=>'w50 wizard'),
+	'sql'                     => "varchar(255) NOT NULL default ''"
+);
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['targetPrimary'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['MSC']['target'],
+	'exclude'                 => true,
+	'inputType'               => 'checkbox',
+	'eval'                    => array('tl_class'=>'w50 m12'),
+	'sql'                     => "char(1) NOT NULL default ''"
+);
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['titleTextPrimary'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_content']['titleText'],
+	'exclude'                 => true,
+	'search'                  => true,
+	'inputType'               => 'text',
+	'eval'                    => array('maxlength'=>255, 'tl_class'=>'w50'),
+	'sql'                     => "varchar(255) NOT NULL default ''"
+);
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['linkTitlePrimary'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_content']['linkTitle'],
+	'exclude'                 => true,
+	'search'                  => true,
+	'inputType'               => 'text',
+	'eval'                    => array('maxlength'=>255, 'tl_class'=>'w50'),
+	'sql'                     => "varchar(255) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_content']['fields']['relPrimary'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_content']['rel'],
+	'exclude'                 => true,
+	'search'                  => true,
+	'inputType'               => 'text',
+	'eval'                    => array('maxlength'=>64, 'tl_class'=>'w50'),
+	'sql'                     => "varchar(64) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_content']['fields']['linkClassPrimary'] = array
+(
+  'label'                   => &$GLOBALS['TL_LANG']['tl_content']['linkClass'],
+	'exclude'                 => true,
+	'search'                  => true,
+	'inputType'               => 'text',
+	'eval'                    => array('maxlength'=>255, 'tl_class'=>'w50'),
+	'sql'                     => "varchar(255) NOT NULL default ''"
+);
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['urlSecondary'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['MSC']['url'],
+	'exclude'                 => true,
+	'search'                  => true,
+	'inputType'               => 'text',
+	'eval'                    => array('rgxp'=>'url', 'decodeEntities'=>true, 'maxlength'=>255, 'dcaPicker'=>true, 'tl_class'=>'w50 wizard'),
+	'sql'                     => "varchar(255) NOT NULL default ''"
+);
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['targetSecondary'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['MSC']['target'],
+	'exclude'                 => true,
+	'inputType'               => 'checkbox',
+	'eval'                    => array('tl_class'=>'w50 m12'),
+	'sql'                     => "char(1) NOT NULL default ''"
+);
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['titleTextSecondary'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_content']['titleText'],
+	'exclude'                 => true,
+	'search'                  => true,
+	'inputType'               => 'text',
+	'eval'                    => array('maxlength'=>255, 'tl_class'=>'w50'),
+	'sql'                     => "varchar(255) NOT NULL default ''"
+);
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['linkTitleSecondary'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_content']['linkTitle'],
+	'exclude'                 => true,
+	'search'                  => true,
+	'inputType'               => 'text',
+	'eval'                    => array('maxlength'=>255, 'tl_class'=>'w50'),
+	'sql'                     => "varchar(255) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_content']['fields']['relSecondary'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_content']['rel'],
+	'exclude'                 => true,
+	'search'                  => true,
+	'inputType'               => 'text',
+	'eval'                    => array('maxlength'=>64, 'tl_class'=>'w50'),
+	'sql'                     => "varchar(64) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_content']['fields']['linkClassSecondary'] = array
+(
+  'label'                   => &$GLOBALS['TL_LANG']['tl_content']['linkClass'],
+	'exclude'                 => true,
+	'search'                  => true,
+	'inputType'               => 'text',
+	'eval'                    => array('maxlength'=>255, 'tl_class'=>'w50'),
+	'sql'                     => "varchar(255) NOT NULL default ''"
+);
---
+++ src/Resources/contao/languages/de/tl_content.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * * This file is part of ErdmannFreunde/euf_hero.
+ *
+ * (c) 2016-2018 Erdmann & Freunde.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package    euf_hero
+ * @author     Dennis Erdmann
+ * @author     Richard Henkenjohann
+ * @copyright  2016-2018 Erdmann & Freunde
+ * @license    LICENSE LGPL-3.0
+ * @filesource
+ */
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_content']['link_primary_legend']			= 'Primäre Verlinkung';
+$GLOBALS['TL_LANG']['tl_content']['link_secondary_legend']			= 'Sekundäre Verlinkung';
+$GLOBALS['TL_LANG']['tl_content']['linkClass']			= array('Link-Klasse','Hier können Sie eine Link-Klasse vergeben, z.B. btn btn--primary');
